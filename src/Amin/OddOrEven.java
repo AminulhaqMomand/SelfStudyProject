7ae3b0f98591b6f5b6480e2355375af782206461
maiyada <maiyada.hasan12@gmail.com>
2023-05-25 21:24:16 -0500

    Added odd or even task by maiyada

---
+++ src/Amin/OddOrEven.java
@@ -0,0 +1,16 @@
+package Amin;
+
+public class OddOrEven {
+
+    public static void main(String[] args) {
+
+        int num = 10;
+
+        if(num % 2 ==0){
+            System.out.println(num + " is even number");
+        }else {
+            System.out.println(num + " is odd number");
+        }
+
+    }
+}
